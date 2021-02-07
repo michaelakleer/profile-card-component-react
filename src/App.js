c6d741767caaa3c2b953d090f9c59e62cfddbb31
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-07 15:02:09 -0600

    Added HTML information

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Victor Crest</h1>
+      <span>26</span>
+      <h2>London</h2>
+      <ul>
+        <li>80K</li>
+        <li>Followers</li>
+      </ul>
+      <ul>
+        <li>803K</li>
+        <li>Likes</li>
+      </ul>
+      <ul>
+        <li>1.4K</li>
+        <li>Photos</li>
+      </ul>
+    </div>
+  );
 }
