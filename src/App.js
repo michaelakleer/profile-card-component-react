b564fced930fd3bc5c827c57aa8a3ee537a010f0
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-08 11:30:34 -0600

    Added divs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Victor Crest</h1>
-      <span>26</span>
-      <h2>London</h2>
-      <ul>
-        <li>80K</li>
-        <li>Followers</li>
-      </ul>
-      <ul>
-        <li>803K</li>
-        <li>Likes</li>
-      </ul>
-      <ul>
-        <li>1.4K</li>
-        <li>Photos</li>
-      </ul>
+      <div className="container">
+        <div className="profile">
+          <h1>Victor Crest</h1>
+          <span>26</span>
+          <h2>London</h2>
+        </div>
+        <hr />
+        <div className="info">
+          <ul>
+            <li>80K</li>
+            <li>Followers</li>
+          </ul>
+          <ul>
+            <li>803K</li>
+            <li>Likes</li>
+          </ul>
+          <ul>
+            <li>1.4K</li>
+            <li>Photos</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
