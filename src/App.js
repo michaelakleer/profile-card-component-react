f5303052fd183bcc5a759a405ea37a61e92baad5
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-08 16:01:03 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
 import "./App.css";
+import Victor from "./images/image-victor.jpg";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <div className="header">
+          <div className="photo">
+            <img src={Victor} alt="Portrait of Victor" className="image" />
+          </div>
+        </div>
         <div className="profile">
           <h1>Victor Crest</h1>
-          <span>26</span>
+          <span className="age">26</span>
           <h2>London</h2>
         </div>
-        <hr />
+
         <div className="info">
           <ul>
-            <li>80K</li>
-            <li>Followers</li>
+            <li className="number">80K</li>
+            <li className="category">Followers</li>
           </ul>
           <ul>
-            <li>803K</li>
-            <li>Likes</li>
+            <li className="number">803K</li>
+            <li className="category">Likes</li>
           </ul>
           <ul>
-            <li>1.4K</li>
-            <li>Photos</li>
+            <li className="number">1.4K</li>
+            <li className="category">Photos</li>
           </ul>
         </div>
       </div>
